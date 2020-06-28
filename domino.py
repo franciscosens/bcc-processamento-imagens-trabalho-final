562320601b0db48e2b73809946018f1934b79348
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2020-06-27 21:22:29 -0300

    Ajustado nome do json

--- domino.py
+++ domino.py
@@ -392,7 +392,9 @@ if __name__ == '__main__':
     if args.weights.lower() == "coco":
         # Exclude the last layers because they require a matching
         # number of classes
-        model.load_weights(weights_path, by_name=True)
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
     else:
         model.load_weights(weights_path, by_name=True)
 
